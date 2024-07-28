7c2140d535c0896207e3e5f761ea64be145d7955
Shamil <shamuilt@gmail.com>
2024-07-28 21:33:39 +0400

    first program

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    generes: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+      b = prompt('На сколько оцениваете его?',''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцениваете его','');
+
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
+
+console.log(personalMoviesDB);
